c96c5a4d3105661059d2c9107841f36fee288391
Maicon <maiconobmepufpa@gmail.com>
2021-10-11 21:20:14 -0400

    :tada: initial commit

---
+++ calculator.js
@@ -0,0 +1,431 @@
+const CALCULATOR = (() => {
+    // -------------------------------BASIC FUNCTIONS------------------------------------------------------
+    const _containsValue = array => value => array.includes(value)
+    const _filter = predicate => array => array.filter(predicate)
+    const _map = callback => array => array.map(callback)
+    const _find = predicate => array => array.find(predicate)
+    const _findIndex = predicate => array => array.findIndex(predicate)
+    const _sort = array => array.sort()
+    const _removeDuplicates = array => Array.from(new Set(array))
+    const _last = array => ([...array].pop())
+    const _upper = str => str.toUpperCase()
+
+    const _getProperty = prop => obj => obj[prop]
+    const _isPropertyEquals = prop => value => obj => obj[prop] == value
+    const _listPropValues = prop => array => _map(_getProperty(prop))(array)
+    const _filterByPropValue = prop => value => array => _filter(_isPropertyEquals(prop)(value))(array)
+    const _findByPropertyValue = prop => array => value => _find(_isPropertyEquals(prop)(value))(array)
+    const _getIndexByValues = values => array => _findIndex(_containsValue(values))(array)
+    const _getIndexByValue = value => array => _findIndex(_containsValue([value]))(array)
+    const _containsAnyValue = values => array => array.some(_containsValue(values))
+    const _getSubArray = (array, start, end) => array.slice(start, end)
+    const _pop = index => array => {
+        const _newArray = [...array]
+        _newArray.splice(index, 1)
+        return _newArray
+    }
+    const _replaceSubarrayByElement = (array, start, len, element) => {
+        const _newArray = [...array]
+        _newArray.splice(start, len, element)
+        return _newArray
+    }
+
+
+    // -------------------------------VARIABLES------------------------------------------------------
+    let historic = []
+    let currentExpression = []
+    let grouperQueue = []
+    let enteredSymbols = []
+    let nextTermsExpected = []
+
+    
+    // -------------------------------UTILITY FUNCTIONS------------------------------------------------------
+    const isValidNumber = value => typeof (value) === "number" && !isNaN(value) && isFinite(value)
+    const getLastDigit = () => _last(currentExpression)
+
+
+    // -------------------------------MATH CONSTANT------------------------------------------------------
+    const factoryMathConstant = (name, value,  symbol) => ({name, value,  symbol})
+
+    const MATH_CONSTANTS = [
+        factoryMathConstant("PI", Math.PI, "\u03C0"),
+        factoryMathConstant("E", Math.E, "\u1D452")
+    ]
+
+    const CONSTANTS = _listPropValues("name")(MATH_CONSTANTS)
+    const isValidConstant = name => _containsValue(CONSTANTS)(_upper(name))
+    const getConstant = name => _findByPropertyValue("name")(MATH_CONSTANTS)(_upper(name))
+
+
+    // -------------------------------OPERATIONS------------------------------------------------------
+    const addOperation = (operand1, operand2) => operand1 + operand2
+    const subOperation = (operand1, operand2) => operand1 - operand2
+    const multOperation = (operand1, operand2) => operand1 * operand2
+    const divOperation = (operand1, operand2) => operand1 / operand2
+    const powOperation = Math.pow
+    const sqrtOperation = Math.sqrt
+    const cbrtOperation = Math.cbrt
+    const squareOperation = operand => powOperation(operand, 2)
+    const cubeOperation = operand => powOperation(operand, 3)
+    const invertOperation = operand => powOperation(operand, -1)
+    const percentOperation = operand => operand/100
+    const factorialOperation = operand => operand < 2 ? 1 : operand * factorialOperation(operand - 1)
+    const oppositeOperation = operand => operand*(-1)
+    const identityOperation = operand => operand
+    const factoryMathOperation = (operator, priority, handler, symbol, position, termExpression, isDuplicate) => {
+        position = (position === undefined) ? "center" : position
+        symbol = (symbol === undefined) ? ` ${operator} `: symbol
+        termExpression = (termExpression === undefined) ? operator : termExpression
+        isDuplicate = (isDuplicate === undefined) ? false : isDuplicate
+        return { operator, priority, handler, symbol, position, termExpression, isDuplicate}
+    }
+    
+    const MATH_OPERATIONS = [
+        factoryMathOperation("+", 3, addOperation),
+        factoryMathOperation("-", 3, subOperation, " \u2212 "),
+        factoryMathOperation("*", 2, multOperation, " \u00D7 "),
+        factoryMathOperation("/", 2, divOperation, " \u00F7 "),
+        factoryMathOperation("^", 1, powOperation, "^"),
+        factoryMathOperation("%", 1, percentOperation, "%", "right"),
+        factoryMathOperation("rz", 1, sqrtOperation, "\u221A", "left"),
+        factoryMathOperation("rz3", 1, cbrtOperation, "\u221B", "left"),
+        factoryMathOperation("!", 0, factorialOperation, "!", "right"),
+        factoryMathOperation("^2", 1, squareOperation, "\u00B2", "right"),
+        factoryMathOperation("^3", 1, cubeOperation, "\u00B3", "right"),
+        factoryMathOperation("^-1", 1, invertOperation, "\u207B\u00B9", "right"),
+        factoryMathOperation("*", 1.5, multOperation, "", "center", "x", true), // Operação em caso de supressão de parênteses
+        factoryMathOperation("-", 1.5, oppositeOperation, "\u2212", "left", "--", true),
+        factoryMathOperation("+", 1.5, identityOperation, "+", "left", "++", true),
+
+    ]
+
+    const PRIMARY_OPERATIONS = _filterByPropValue("isDuplicate")(false)(MATH_OPERATIONS)
+    const SECONDARY_OPERATIONS = _filterByPropValue("isDuplicate")(true)(MATH_OPERATIONS)
+
+    // PRIMARY OPERATION GENERIC FUNCTIONS
+    const getTermExpressionByProperty = prop => operations => value => _listPropValues("termExpression")(_filterByPropValue(prop)(value)(operations))
+    const getOperationByTermExpression = _findByPropertyValue("termExpression")(MATH_OPERATIONS)
+    const getPryOperationByOperator = _findByPropertyValue("operator")(PRIMARY_OPERATIONS)
+    const getSecOperationByOperator = _findByPropertyValue("operator")(SECONDARY_OPERATIONS)
+    const getOperatorsByPriority = getTermExpressionByProperty("priority")(MATH_OPERATIONS)
+    const getOperatorsByPosition = getTermExpressionByProperty("position")
+    const getOperatorIndexes = (mathExpression, operators) => _getIndexByValues(operators)(mathExpression)
+
+    // OPERATION CONSTANTS
+    const OPERATION_PRIORITIES = _sort(_removeDuplicates(_listPropValues("priority")(MATH_OPERATIONS)))
+    const CENTER_OPERATORS = getOperatorsByPosition(MATH_OPERATIONS)("center")
+    const LEFT_OPERATORS = getOperatorsByPosition(MATH_OPERATIONS)("left")
+    const RIGHT_OPERATORS = getOperatorsByPosition(MATH_OPERATIONS)("right")
+    const OPERATORS = _removeDuplicates([...LEFT_OPERATORS, ...CENTER_OPERATORS, ...RIGHT_OPERATORS])
+    const SECONDARY_OPERATORS = _listPropValues("operator")(SECONDARY_OPERATIONS)
+
+    // OPERATION SPECIFIC FUNCTIONS
+    const isValidSecOperator = _containsValue(SECONDARY_OPERATORS)
+    const isValidOperator = _containsValue(OPERATORS)
+    const isValidRightOperator = _containsValue(RIGHT_OPERATORS)
+
+
+    // -------------------------------GROUPERS------------------------------------------------------
+    const factoryMathGroupers = (name, left, right, priority) => ({name, left, right, priority })
+    const mathGroupers = [
+        factoryMathGroupers("parentheses", "(", ")", 3),
+        factoryMathGroupers("bracket", "[", "]", 2),
+        factoryMathGroupers("brace", "{", "}", 1),
+    ]
+
+    // GROUPER GENERIC FUNCTIONS
+    const getGroupersPropertyValue = prop => _listPropValues(prop)(mathGroupers)
+    const getGrouperByPropertyValue = prop => value => _findByPropertyValue(prop)(mathGroupers)(value)
+    const getLastGrouper = () => _last(grouperQueue)
+
+    // GROUPER CONSTANTS
+    const GROUPER_PRIORITIES = _sort(_removeDuplicates(_listPropValues("priority")(mathGroupers)))
+    const LETF_GROUPERS = getGroupersPropertyValue("left")
+    const RIGHT_GROUPERS = getGroupersPropertyValue("right")
+    const GROUPERS = [...LETF_GROUPERS, ...RIGHT_GROUPERS]
+
+    // GROUPER SPECIFIC FUNCTIONS
+    const isValidGrouper = _containsValue(GROUPERS)
+    const isValidLeftGrouper = _containsValue(LETF_GROUPERS)
+    const isValidRightGrouper = _containsValue(RIGHT_GROUPERS)
+    const getGrouperByLeft = getGrouperByPropertyValue("left")
+    const getGrouperByRight = getGrouperByPropertyValue("right")
+    const getGrouperByPriority = getGrouperByPropertyValue("priority")
+    const getGrouperStartIndex = (mathExpression, leftGrouper) => _getIndexByValue(leftGrouper)(mathExpression)
+    const getGrouperEndIndex = (mathExpression, grouperNumber) => _getIndexByValue(grouperNumber)(mathExpression)
+    const isThereGrouperIn = _containsAnyValue(GROUPERS)
+
+    // -------------------------------CALCULATE FUNCTIONS------------------------------------------------------
+    const resolveOperation = (mathExpression, operatorIndex) => {
+        const operatorTermExpression = mathExpression[operatorIndex]
+        const operation = getOperationByTermExpression(operatorTermExpression)
+        const operands = []
+        let startStepIndex = 0
+        let lenStepEx = 0
+
+        switch (operation.position) {
+            case "center": {
+                operands.push(mathExpression[operatorIndex - 1])
+                operands.push(mathExpression[operatorIndex + 1])
+                startStepIndex = operatorIndex - 1
+                break
+            }
+            case "right": {
+                operands.push(mathExpression[operatorIndex - 1])
+                startStepIndex = operatorIndex - 1
+                break
+            }
+            case "left": {
+                const operand = mathExpression[operatorIndex + 1]
+                if (isValidNumber(operand)) {
+                    operands.push(operand)
+                    startStepIndex = operatorIndex
+                    break
+                }
+                else {
+                    return resolveOperation(mathExpression, operatorIndex + 1)
+                }
+            }
+        }
+
+        lenStepEx = operands.length + 1
+        result = operation.handler(...operands)
+        return _replaceSubarrayByElement(mathExpression, startStepIndex, lenStepEx, result)
+    }
+
+    const resolveSelectOperations = (mathExpression, operators) => {
+        let newMathExpression = [...mathExpression]
+        let currentOperatorIndex = getOperatorIndexes(newMathExpression, operators)
+
+        while (currentOperatorIndex !== -1) {
+            newMathExpression = resolveOperation(newMathExpression, currentOperatorIndex)
+            currentOperatorIndex = getOperatorIndexes(newMathExpression, operators)
+        }
+
+        return newMathExpression
+    }
+
+    const resolveSubExpression = (mathSubExpression) => {
+        let result = [...mathSubExpression]
+
+        for (priority of OPERATION_PRIORITIES) {
+            const currentAbleOperators = getOperatorsByPriority(priority)
+            result = resolveSelectOperations(result, currentAbleOperators)
+
+            if (result.length == 1) {
+                return result[0]
+            }
+        }
+    }
+
+    const resolveGrouper = (mathExpression, start) => {
+        const grouperNumber = mathExpression[start-1]
+        let _mathExpression = _pop(start-1)(mathExpression) // REMOVE NAME START GROUPER
+
+        const end = getGrouperEndIndex(_mathExpression, grouperNumber)
+        _mathExpression = _pop(end)(_mathExpression) // REMOVE NAME END GROUPER
+
+        const subexpression = _getSubArray(_mathExpression, start, end)
+        const result = resolveExpression(subexpression)
+        
+        return _replaceSubarrayByElement(_mathExpression, start-1, end - start + 2, result)
+    }
+
+    const resolveSubExpressionsByGrouper = (mathExpression, grouper) => {
+        let expression = [...mathExpression]
+        let nextIndexStartGrouper = getGrouperStartIndex(expression, grouper.left)
+
+        while(nextIndexStartGrouper !== -1){
+            expression = resolveGrouper(expression, nextIndexStartGrouper)
+            nextIndexStartGrouper = getGrouperStartIndex(expression, grouper.left)
+        }
+
+        return expression
+    }
+
+    const resolveExpression = (mathExpression) => {
+        let expression = [...mathExpression]
+
+        if (isThereGrouperIn(expression)) {
+            for (priority of GROUPER_PRIORITIES) {
+                const currentGrouper = getGrouperByPriority(priority)
+                expression = resolveSubExpressionsByGrouper(expression, currentGrouper)
+            }
+        }
+
+        return resolveSubExpression(expression)
+    }
+
+
+    // -------------------------------ENTER FUNCTIONS--------------------------------------------------
+    const _addSuppression = () => currentExpression.push("x")
+
+    const nextTermExpressionExpected = (currentTermType, currentTermPosition) => {
+        switch(currentTermType){
+            case "grouper":
+                const commom = ["número"]
+
+                switch(currentTermPosition){
+                    case "left":
+                        return [...commom, ...LEFT_OPERATORS, ...LETF_GROUPERS]
+                    case "right":
+                        return [...commom, ...RIGHT_OPERATORS, ...CENTER_OPERATORS, ...RIGHT_GROUPERS]
+                }
+            case "operator":
+                switch(currentTermPosition){
+                    case "left":
+                        return ["número", ...LEFT_OPERATORS, ...LETF_GROUPERS]
+                    case "center":
+                        return ["número", ...LEFT_OPERATORS, ...LETF_GROUPERS]
+                    case "right":
+                        return [...OPERATORS, ...GROUPERS]
+                }
+            case "número":
+                return [...OPERATORS, ...GROUPERS]
+            case undefined:
+                return ["número", ...LEFT_OPERATORS, ...LETF_GROUPERS]
+        }
+    }
+
+    const enterGrouperDigit = input => {
+        const lastGrouper = getLastGrouper()
+        let mathGrouper = undefined
+        let numberGrouper = 0
+        let typeGrouper = undefined
+        const lastDigit = getLastDigit()
+
+        if(!_containsValue(nextTermsExpected)(input)){
+            throw Error(`É esperado um dos termos: ${nextTermsExpected.join(", ")}`)
+        }
+
+        if(lastGrouper === undefined || isValidLeftGrouper(input)){
+            typeGrouper = "left"
+            mathGrouper = getGrouperByLeft(input)
+            grouperQueue.push(input)
+            numberGrouper = grouperQueue.length
+
+            if(isValidNumber(lastDigit) || isValidRightOperator(lastDigit)){
+                _addSuppression()
+            }
+        }
+        else{
+            mathGrouper = getGrouperByRight(input)
+            typeGrouper = "right"
+            if (mathGrouper.left !== lastGrouper){
+                throw Error(`É esperado o fechamento do agrupador ${lastGrouper}.`) 
+            }
+
+            numberGrouper = grouperQueue.length
+            grouperQueue.pop()
+        }
+        currentExpression.push(`${mathGrouper.name}${numberGrouper}`) // ADD NAME GROUPER
+        nextTermsExpected = nextTermExpressionExpected("grouper", typeGrouper)
+    }
+
+    
+    const getOperationByDigit = input =>{
+        let operation = getPryOperationByOperator(input)
+
+        if(_containsValue(nextTermsExpected)(operation.termExpression)){
+            return operation
+        }
+        if(isValidSecOperator(input)){
+            operation = getSecOperationByOperator(input)
+
+            if(_containsValue(nextTermsExpected)(operation.termExpression)){
+                return operation
+            }
+        }
+    }
+
+    const enterOperatorDigit = input => {
+        let operation = getOperationByDigit(input)
+
+        if(operation === undefined){
+            throw Error(`É esperado um dos termos: ${nextTermsExpected.join(", ")}`)
+        }
+
+        if(operation.position === "left"){
+            const lastDigit = getLastDigit()
+            const isValidSuppression = isValidNumber(lastDigit) ||
+                                        isValidRightGrouper(lastDigit) ||
+                                        isValidRightOperator(lastDigit)
+            if(isValidSuppression){
+                _addSuppression()
+            }
+        }
+
+        nextTermsExpected = nextTermExpressionExpected("operator", operation.position)
+        return operation
+    }
+
+    const enterNumberDigit = input => {
+        const lastDigit = getLastDigit()
+
+        if(!_containsValue(nextTermsExpected)("número")){
+            throw Error(`É esperado um dos termos: ${nextTermsExpected.join(", ")}`)
+        }
+        
+        if (isValidRightGrouper(lastDigit)){
+            _addSuppression()
+        }
+        nextTermsExpected = nextTermExpressionExpected("número")
+    }
+
+
+    // ----------------------------------MAIN FUNCTIONS--------------------------------------------------
+    const enter = (input) => {
+        let symbol = input
+        let termExpression = input
+
+        if(nextTermsExpected.length === 0){
+            nextTermsExpected = nextTermExpressionExpected()
+        }
+
+        if (isValidGrouper(input)){
+            enterGrouperDigit(input)
+        }
+        else if (isValidOperator(input)){
+            const operation = enterOperatorDigit(input)
+            termExpression = operation.termExpression
+            symbol = operation.symbol
+        }
+        else if (isValidNumber(input)){
+            enterNumberDigit(input)
+        }
+        else if (isValidConstant(input)){
+            const constant = getConstant(input)
+            enterNumberDigit(input)
+            symbol = constant.symbol
+            termExpression = constant.value
+        }
+        else{
+            throw Error("Dígito inválido.")
+        }
+        
+        enteredSymbols.push(symbol)
+        currentExpression.push(termExpression)
+    }
+
+    const equals = () => {
+        const result = resolveExpression(currentExpression)
+        historic.push(`${enteredSymbols.join("")} = ${result}`)
+        currentExpression = []
+        enteredSymbols = []
+        nextTermsExpected = []
+        return result
+    }
+    const list = () => historic
+    const reset = () => currentExpression = []
+    const clear = () => historic = []
+
+
+    return {
+        enter,
+        equals,
+        list,
+        reset,
+        clear
+    }
+}
+)()
---
+++ readme.md
@@ -0,0 +1,152 @@
+# :abacus: MW CALC
+### Aqui está uma calculadora implementada em JS que promete resolver operações matemáticas de nível intermediário :astonished:. Não chega a ser uma calculadora científica, mas está cima de uma de "padaria" :stuck_out_tongue_winking_eye:.
+
+![Badge](https://img.shields.io/badge/Java%20Script-%B1Calculadora-F27405?style=for-the-badge&logo=javascript)
+
+
+:checkered_flag: Conteúdo 
+-----------------------
+- [Sobre](#sobre)
+- [Como usar](#como-usar)
+    - [Execução](#execução)
+    - [Funções](#funções)
+    - [Constantes](#constantes)
+    - [Operações](#operações)
+    - [Agrupadores](#agrupadores)
+    - [Exemplos](#exemplos)
+- [Funcionalidades](#funcionalidades)
+- [Futuro](#futuro)
+
+## Sobre
+Este código foi desenvolvido inicialmente como parte avaliativa do curso @Santander Lets Code - Full Stack Develop, mas o interesse pela linguagem e desafios enfrentados para implementação de funcionalidades além do cobrado, fez este desenvolvedor ir "um pouco" além. 
+
+Trata-se de um código em único script, em java script puro e sem uso do temido <code> eval </code>. Só foi utilizado um pouco de café, horas se torturando com alguns detalhes e, quando lembrado, os fundamentos de programção funcional em javascript.
+
+## Como Usar
+### Execução
+No node, pode-se realizar o comando
+    
+    node calculator
+... e tudo estará bem ...
+
+
+### Funções
+Como módulo inicializado, temos em mãos as seguintes funções:
+
++ **enter**: realiza a entrada de um novo elemento à expressão matemática;
++ **equals**: resolve a expressão matemática;
++ **list**: lista o histórico de operações;
++ **reset**: reiniciar uma operação;
++ **clear**: limpa o histórico de operações.
+
+
+### Constantes
+É possível se utlizar de constantes matemáticas:
+
+| Constante       | Digitalização <br />(case isensitive) | Símbolo   
+| ----------------|:-------------------------------:|:------------:
+| Pi              | pi                              | &#x03C0;
+| Número de Euler | e                               | &#x1D452;
+
+
+### Operações
+Esta calculadora implementa as seguintes operações
+
+| Operação        | Operador   | Descrição                                            | Prioridade   | Tipo    | Posição 
+| ----------------|:----------:|------------------------------------------------------|:------------:|:-------:|:---------:
+| Soma            | +          | Soma dois números quaisquer.                         | 3            | binária | centro
+| Subtração       | -          | Subtrai dois números quaisquer.                      | 3            | binária | centro
+| Multiplicação   | *          | Multiplica dois números quaisquer.                   | 2            | binária | centro
+| Divisão         | /          | Dividi dois números quaisquer, mas não fazemos divisão por aqui... | 2            | binária | centro
+| Potenciação     | ^          | Realiza a potenciação de dois números quaisquer, quando possível ....      | 1            | binária | centro
+| Raiz quadrada   | rz         | Calcula a raiz quadrada de um número não negativo.   | 1            | unária  | esquerda
+| Raiz cúbica     | rz3        | Calcula a raiz cúbica de um número qualquer.         | 1            | unária  | esquerda
+| Quadrado        | ^2         | Calcula o quadrado de um número qualquer.            | 1            | unária  | direita
+| Cubo            | ^3         | Calcula o cubo de um número qualquer.                | 1            | unária  | direita
+| Inversão        | ^-1        | Calcula o inverso multiplicativo de número não nulo (numerador e denomidaor "trocados"). | 1            | unária  | direita
+| Porcentagem     | %          | Realiza a fivião de um número qualquer por 100.      | 1            | unária  | direita
+| Fatorial        | !          | Calcula o fatorial de um número inteiro positivo.    | 0            | unária  | direita
+| Supressão       |            | É o utilizada quando é suprimido o uso de parênteses.| 1.5          | binária | centro
+| Oposição        | -          | Calcula o oposto de um número qualquer ("inverte o sinal").  | 1.5          | unária  | esquerda
+| Identidade      | +          | Apenas retorna o próprio operando.                   | 1.5          | unária  | esquerda
+
+A ordem de precedências seguirá das operações com menor ordinalidade em prioridade para as de maior valor, ou seja, do 0 ao 3.
+
+### Agrupadores
+Os agrupadores ariméticos implementados são descritos abaixo:
+
+| Agrupamento     | Agrupadores  | Prioridade   
+| ----------------|:------------:|:------------:
+| Parênteses      | ( e )        | 3            
+| Colchetes       | [ e ]        | 2           
+| Chaves          | { e }        | 1        
+
+Da mesma forma que em operações, os agrupadores são resolvidos em uma expresão em ordem de prioridade, indo do 1 ao 3.
+
+### Exemplos
+Podemos executra uma operação inputando os termos da expressão de acordo com as tabelas de operadores, constantes e agrupadores acima e seguido de um **equals**:
+
+```js
+CALCULATOR.enter(2)
+CALCULATOR.enter('+')
+CALCULATOR.enter(4)
+CALCULATOR.equals()
+6
+```
+```js
+CALCULATOR.enter(3)
+CALCULATOR.enter('!')
+CALCULATOR.enter('!')
+CALCULATOR.equals()
+720
+```
+Um modo mais cômodo é criando uma função que abstrai as entradas e a saída das operações...
+```js
+function calculate(...inputs){
+    inputs.map(CALCULATOR.enter)
+    return CALCULATOR.equals()
+}
+```
+Assim podemos compor operações mais complexas sem muito trabalho manual...
+```js
+calculate('(', 128, '/', 8, ')', '^', 2, '+', 7, '-', '(', 3, '*', 4, ')', '^', 2)
+calculate('[', '(', '(', 128, '/', 8, ')', '^', 2, '+', 7, '-', '(', 3, '*', 4, ')', '^', 2, ')', '+', 1, ']', '/', 4, '!')
+calculate(28, '/', '(', 2, '+', 5, ')', 2)
+calculate(3, '^', '(', 'rz', '(', 3, '*', 12, ')' , 0.5, ')')
+calculate(2, 'rz', 16)
+calculate('-', 3, '^', 2)
+calculate('(', '-', 3, ')', '^2')
+calculate('+', '-', '-', '+', 'pi')
+
+CALCULATOR.list()
+
+[ '(128 ÷ 8)^2 + 7 − (3 × 4)^2 = 119',
+  '[((128 ÷ 8)^2 + 7 − (3 × 4)^2) + 1] ÷ 4! = 5',
+  '28 ÷ (2 + 5)2 = 2',
+  '3^(√(3 × 12)0.5) = 27',
+  '2√16 = 8',
+  '−3² = -9',
+  '(−3)² = 9',
+    '+−−+π = 3.141592653589793' ]
+```
+## Funcionalidades
+- [x] Operações básicas (+, &#x2212;, &#x00D7;, &#x00F7;, ^);
+- [x] Ordem de precedência;
+- [x] Uso de agrupadores ((), [], {});
+- [x] Operações à diretia e à esquerda (!, %, &#x221A;, &#x00B2;, &#x00B3;, &#x207B;&#x00B9;);
+- [x] Uso de supressão de parênteses;
+- [x] Sobreposição de operações ('-' para inversão e '+' para identidade);
+- [x] Uso de constantes matemáticas (&#x03C0;, &#x1D452;);
+- [ ] Funções matemáticas(módulo, trigonométricas, logaritimos) :collision:;
+- [ ] Configurações de medidas de ângulo;
+- [ ] Conversão de medidas;
+- [ ] Notação de engenharia e científica;
+- [ ] Uso de variáveis (x, y);
+- [ ] Operação com acumuladores (somatórios).
+
+## Futuro
+Pretende-se implementar uma interface gráfica com variações de calculadoras (básica, científica e lógica), mas isso será após eu consegui mais café :coffee::yum:....
+
+
+
+
